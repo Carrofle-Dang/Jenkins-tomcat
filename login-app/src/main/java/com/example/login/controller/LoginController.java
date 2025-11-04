dcec5fc9fa9124ae8a967fe645546d94506b519e
Carrofle-Dang <45886715+Carrofle-Dang@users.noreply.github.com>
2025-11-04 17:00:18 +0900

    Create LoginController.java

---
+++ login-app/src/main/java/com/example/login/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.example.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+    @GetMapping("/home")
+    public String homePage() {
+        return "home";
+    }
+   @GetMapping("/register")
+    public String homePage() {
+        return "register";
+    }
+}
